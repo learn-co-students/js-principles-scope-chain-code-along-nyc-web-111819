fc7e599f1035447e697dce57a71aba34a4ead4f8
kevinkim1030 <kevin.kim1030@gmail.com>
2020-01-04 20:30:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 /* Your code goes here */
+var customerName = 'bob';
+const leastFavoriteCustomer = 'kevin';
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer(){
+  bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'kevina';
+}
+
+function attemptTwoFavoriteCustomers(){
+  // const favoriteCustomer = 'you';
+  let favoriteCustomer = 'me';
+  let favoriteCustomer = 'me';
+  // let favoriteCustomer = 'you';
+}
